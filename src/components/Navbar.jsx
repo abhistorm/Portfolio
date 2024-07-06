a30deb52f0595dab561c9f5fdd0e5f13aea1f879
Abhinav Gupta <74293846+abhistorm@users.noreply.github.com>
2024-07-06 15:28:37 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -113,7 +113,7 @@ const Navbar = () => {
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                         <a
                             className='flex justify-between items-center w-full text-gray-300'
-                            href='hhttps://github.com/abhistorm/'
+                            href='https://github.com/abhistorm/'
                         >
                             Github <FaGithub size={30} />
                         </a>
